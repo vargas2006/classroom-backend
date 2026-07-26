4d78720c634019e4474cd52a2392294d6a68ed1d
vargas2006 <johnleevargas25@gmail.com>
2026-07-26 15:44:31 +0800

    make port dynamic using process.env.PORT

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { auth } from './lib/auth.js';
 
 
 const app = express();
-const PORT  = 8000;
+const PORT  = process.env.PORT || 8000;
 if (!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not set in .env file');
 app.use(cors({
     origin: process.env.FRONTEND_URL,
